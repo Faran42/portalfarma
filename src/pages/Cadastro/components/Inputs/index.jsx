7454ebb75931444f6fd5e4e13f4a23c5c7a7f4d2
Faran42 <faranorphys@gmail.com>
2021-08-23 22:35:44 -0300

    just sending to repo

--- src/pages/Cadastro/components/Inputs/index.jsx
+++ src/pages/Cadastro/components/Inputs/index.jsx
@@ -1,13 +1,21 @@
 import React from "react";
+import { useEffect } from "react";
 
 import { Container, Input, Label } from "./styles";
 
 export function Inputs({ label, type, inputState, setInputState }) {
-  
+  function handleOnChange(event) {
+    setInputState(event.target.value);
+  }
+
+  useEffect(()=> {
+    console.log(`${label}: `, inputState )
+  }, [inputState])
+
   return (
     <Container>
       <Label>{label}:</Label>
-      <Input type={type} onChange={(e) => console.log(e.target.value)}/>
+      <Input type={type} onChange={(e) => handleOnChange(e)} />
     </Container>
   );
 }
--- src/pages/Cadastro/index.jsx
+++ src/pages/Cadastro/index.jsx
@@ -6,6 +6,10 @@ import { Inputs } from "./components/Inputs";
 
 export function Cadastro() {
   const [medicamento, setMedicamento] = useState("");
+  const [medico, setMedico] = useState("");
+  const [quantidade, setQuantidade] = useState(0);
+  const [reqDate, setReqDate] = useState(Date);
+  const [phone, setPhone] = useState(0);
   return (
     <Container>
       <Card>
@@ -16,10 +20,30 @@ export function Cadastro() {
           inputState={medicamento}
           setInputState={setMedicamento}
         />
-        <Inputs type="text" label="Médico" />
-        <Inputs type="number" label="Quantidade" />
-        <Inputs type="date" label="Requisição" />
-        <Inputs type="phone" label="Telefone" />
+        <Inputs
+          type="text"
+          label="Médico"
+          inputState={medico}
+          setInputState={setMedico}
+        />
+        <Inputs
+          type="number"
+          label="Quantidade"
+          inputState={quantidade}
+          setInputState={setQuantidade}
+        />
+        <Inputs
+          type="date"
+          label="Requisição"
+          inputState={reqDate}
+          setInputState={setReqDate}
+        />
+        <Inputs
+          type="phone"
+          label="Telefone"
+          inputState={phone}
+          setInputState={setPhone}
+        />
       </Card>
     </Container>
   );

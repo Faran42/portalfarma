166509cdac1095d5ff3b55fcfc2adc007f363885
Faran42 <faranorphys@gmail.com>
2021-09-14 17:59:15 -0300

    logout button

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,22 +1,35 @@
 import React from "react";
 import { Container, Text, Avatar, Wrapper1, Wrapper2 } from "./styles";
-
+import { useGoogleLogout } from 'react-google-login'
+import { useHistory } from 'react-router-dom';
 import { NavLink } from '../../components/NavLink'
 
 export function Header() {
+  
+  const { signOut, loaded } = useGoogleLogout({})
+
+  const history = useHistory();
   const username = sessionStorage.getItem("givenName");
   const avatarUrl = sessionStorage.getItem("imageUrl");
   
+  function handleClick(){
+    return(
+      signOut(), 
+      history.push("/")
+    )
+  }
   return (
     <Container>
       <Wrapper1>
-        <NavLink href='dashboard' title='Home'/>
-        <NavLink href='cadastrar' title='Cadastrar'/>
-        <NavLink href='requisicoes' title='Requisições'/>
+        <NavLink href='dashboard' title='Home' />
+        <NavLink href='cadastrar' title='Cadastrar' />
+        <NavLink href='requisicoes' title='Requisições' />
       </Wrapper1>
       <Wrapper2>
         <Text>Olá, {username}</Text>
-        <Avatar src={avatarUrl} />
+        <button onClick={handleClick}>
+          <Avatar src={avatarUrl} />
+        </button>
       </Wrapper2>
     </Container>
   );
